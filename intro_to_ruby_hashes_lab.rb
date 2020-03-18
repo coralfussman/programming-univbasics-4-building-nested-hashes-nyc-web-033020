72daa5dcb7d5e4b6ed24ed497b40405ee814435b
Coral Fussman <coralfussman@gmail.com>
2020-03-18 02:52:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,10 +31,7 @@ def monopoly_with_third_tier
                          :three_pieces_owned => 100,
                          :two_pieces_owned => 200
                         },
-    :names => {:reading_railroad => {},
-               :pennsylvania_railroad => {},
-               :b_and_o_railroad => {},
-               :shortline_railroad => {}
+    :names => {
              }
   }}
 
